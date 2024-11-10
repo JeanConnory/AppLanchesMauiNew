ccf413ab758bfb4063f913f7771228a93fb033c9
Jean.Araujo <jean.rodrigues@sepog.fortaleza.ce.gov.br>
2024-11-10 11:37:52 -0300

    tela de produtos, carrinho, detalhes de itens concluidas

---
+++ AppLanches/Pages/ProdutoDetalhePage.xaml.cs
@@ -0,0 +1,135 @@
+using AppLanches.Models;
+using AppLanches.Services;
+using AppLanches.Validations;
+
+namespace AppLanches.Pages;
+
+public partial class ProdutoDetalhePage : ContentPage
+{
+    private readonly ApiService _apiService;
+    private readonly IValidator _validator;
+    private int _produtoId;
+    private bool _loginPageDisplayed = false;
+
+    public ProdutoDetalhePage(int produtoId, string produtoNome, ApiService apiService, IValidator validator)
+    {
+        InitializeComponent();
+        _apiService = apiService;
+        _validator = validator;
+        _produtoId = produtoId;
+        Title = produtoNome ?? "Detalhes do Produto";
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await GetProdutoDetalhes(_produtoId);
+    }
+
+    private async Task<Produto?> GetProdutoDetalhes(int produtoId)
+    {
+        var (produtoDetalhe, errorMessage) = await _apiService.GetProdutoDetalhe(produtoId);
+
+        if (errorMessage == "Unauthorized" && !_loginPageDisplayed)
+        {
+            await DisplayLoginPage();
+            return null;
+        }
+
+        if (produtoDetalhe is null)
+        {
+            await DisplayAlert("Erro", errorMessage ?? "Não foi possível obter o produto.", "OK");
+            return null;
+        }
+
+        if (produtoDetalhe is not null)
+        {
+            ImagemProduto.Source = produtoDetalhe.CaminhoImagem;
+            LblProdutoNome.Text = produtoDetalhe.Nome;
+            LblProdutoPreco.Text = produtoDetalhe.Preco.ToString();
+            LblProdutoDescricao.Text = produtoDetalhe?.Detalhe;
+            LblPrecoTotal.Text = produtoDetalhe?.Preco.ToString();
+        }
+        else
+        {
+            await DisplayAlert("Erro", errorMessage ?? "Não foi possível obter os detalhes do produto.", "OK");
+            return null;
+        }
+
+        return produtoDetalhe;
+    }
+
+    private async Task DisplayLoginPage()
+    {
+        _loginPageDisplayed = true;
+        await Navigation.PushAsync(new LoginPage(_apiService, _validator));
+    }
+
+    private void ImagemBtnFavorito_Clicked(object sender, EventArgs e)
+    {
+
+    }
+
+    private void BtnRemove_Clicked(object sender, EventArgs e)
+    {
+        if (int.TryParse(LblQuantidade.Text, out int quantidade) && decimal.TryParse(LblProdutoPreco.Text, out decimal precoUnitario))
+        {
+            quantidade = Math.Max(1, quantidade - 1);
+            LblQuantidade.Text = quantidade.ToString();
+
+            var precoTotal = quantidade * precoUnitario;
+            LblPrecoTotal.Text = precoTotal.ToString();
+        }
+        else
+        {
+            DisplayAlert("Erro", "Valores inválidos", "OK");
+        }
+    }
+
+    private void BtnAdiciona_Clicked(object sender, EventArgs e)
+    {
+        if (int.TryParse(LblQuantidade.Text, out int quantidade) && decimal.TryParse(LblProdutoPreco.Text, out decimal precoUnitario))
+        {
+            quantidade++;
+            LblQuantidade.Text = quantidade.ToString();
+
+            var precoTotal = quantidade * precoUnitario;
+            LblPrecoTotal.Text = precoTotal.ToString();
+        }
+        else
+        {
+            DisplayAlert("Erro", "Valores inválidos", "OK");
+        }
+    }
+
+    private async void BtnIncluirNoCarrinho_Clicked(object sender, EventArgs e)
+    {
+        try
+        {
+            var carrinhoCompra = new CarrinhoCompra()
+            {
+                Quantidade = Convert.ToInt32(LblQuantidade.Text),
+                PrecoUnitario = Convert.ToDecimal(LblProdutoPreco.Text),
+                ValorTotal = Convert.ToDecimal(LblPrecoTotal.Text),
+                ProdutoId = _produtoId,
+                ClienteId = Preferences.Get("usuarioid", 0)
+            };
+
+            var response = await _apiService.AdicionarItemNoCarrinho(carrinhoCompra);
+
+            if (response.Data)
+            {
+                await DisplayAlert("Sucesso", "Item adicionado ao carrinho!", "OK");
+                await Navigation.PopAsync();
+            }
+            else
+            {
+                await DisplayAlert("Erro", $"Falha ao adicionar item: {response.ErrorMessage}", "OK");
+            }
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Erro", $"Ocorreu um erro: {ex.Message}", "OK");
+        }
+    }
+}
--- AppLanches/Services/ApiService.cs
+++ AppLanches/Services/ApiService.cs
@@ -1,6 +1,7 @@
 ﻿using AppLanches.Models;
 using Microsoft.Extensions.Logging;
 using System.Net;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
 
@@ -9,7 +10,6 @@ namespace AppLanches.Services;
 public class ApiService
 {
     private readonly HttpClient _httpClient;
-    public string _baseUrl = "https://s76c084n-7066.brs.devtunnels.ms/";
     private readonly ILogger<ApiService> _logger;
     JsonSerializerOptions _serializerOptions;
 
@@ -85,7 +85,7 @@ public class ApiService
             var jsonResult = await response.Content.ReadAsStringAsync();
             var result = JsonSerializer.Deserialize<Token>(jsonResult, _serializerOptions);
 
-            Preferences.Set("acesstoken", result!.AccessToken);
+            Preferences.Set("accesstoken", result!.AccessToken);
             Preferences.Set("usuarioid", (int)result!.UsuarioId!);
             Preferences.Set("usuarionome", result.UsuarioNome);
 
@@ -98,9 +98,59 @@ public class ApiService
         }
     }
 
+    public async Task<ApiResponse<bool>> AdicionarItemNoCarrinho(CarrinhoCompra carrinhoCompra)
+    {
+        try
+        {
+            var json = JsonSerializer.Serialize(carrinhoCompra, _serializerOptions);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            var response = await PostRequest("api/ItensCarrinhoCompra", content);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                _logger.LogError($"Erro ao enviar requisição HTTP: {response.StatusCode}");
+                return new ApiResponse<bool>
+                {
+                    ErrorMessage = $"Erro ao enviar requisição HTTP: {response.StatusCode}"
+                };
+            }
+
+            return new ApiResponse<bool> { Data = true };
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Erro ao adicionar item no carrinho: {ex.Message}");
+            return new ApiResponse<bool> { ErrorMessage = ex.Message };
+        }
+    }
+
+    public async Task<ApiResponse<bool>> ConfirmarPedido(Pedido pedido)
+    {
+        try
+        {
+            var json = JsonSerializer.Serialize(pedido, _serializerOptions);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+
+            var response = await PostRequest("api/pedidos", content);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                string errorMessage = response.StatusCode == HttpStatusCode.Unauthorized ? "Unauthorized" : $"Erro ao enviar requisição HTTP: {response.StatusCode}";
+                _logger.LogError(errorMessage);
+                return new ApiResponse<bool> { ErrorMessage = errorMessage };
+            }
+            return new ApiResponse<bool> { Data = true };
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Erro ao confirmar pedido: {ex.Message}");
+            return new ApiResponse<bool> { ErrorMessage = ex.Message };
+        }
+    }
+
     private async Task<HttpResponseMessage> PostRequest(string uri, HttpContent content)
     {
-        var enderecoUrl = _baseUrl + uri;
+        var enderecoUrl = AppConfig.BaseUrl + uri;
         try
         {
             var result = await _httpClient.PostAsync(enderecoUrl, content);
@@ -110,6 +160,134 @@ public class ApiService
         {
             _logger.LogError($"Erro no login: {ex.Message}");
             return new HttpResponseMessage(HttpStatusCode.BadRequest);
+        }
+    }
+
+    public async Task<(List<Categoria>? Categorias, string? ErrorMessage)> GetCategorias()
+    {
+        return await GetAsync<List<Categoria>>("api/categorias");
+    }
+
+    public async Task<(List<Produto>? Produtos, string? ErrorMessage)> GetProdutos(string tipoProduto, string categoriaId)
+    {
+        string endpoint = $"api/Produtos?tipoProduto={tipoProduto}&CategoriaId={categoriaId}";
+        return await GetAsync<List<Produto>>(endpoint);
+    }
+
+    public async Task<(Produto? ProdutoDetalhe, string? ErrorMessage)> GetProdutoDetalhe(int produtoId)
+    {
+        string endpoint = $"api/produtos/{produtoId}";
+        return await GetAsync<Produto>(endpoint);
+    }
+
+    public async Task<(List<CarrinhoCompraItem>? carrinhoCompraItems, string? ErroMessage)> GetItensCarrinhoCompra(int usuarioId)
+    {
+        var endpoint = $"api/ItensCarrinhoCompra/{usuarioId}";
+        return await GetAsync<List<CarrinhoCompraItem>>(endpoint);
+    }
+
+    private async Task<(T? Data, string? ErrorMessage)> GetAsync<T>(string endpoint)
+    {
+        try
+        {
+            AddAuthorizationHeader();
+            var response = await _httpClient.GetAsync(AppConfig.BaseUrl + endpoint);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseString = await response.Content.ReadAsStringAsync();
+                var data = JsonSerializer.Deserialize<T>(responseString, _serializerOptions);
+                return (data ?? Activator.CreateInstance<T>(), null);
+            }
+            else
+            {
+                if (response.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    string errorMessage = "Unauthorized";
+                    _logger.LogWarning(errorMessage);
+                    return (default, errorMessage);
+                }
+
+                string generalErrorMessage = $"Erro na requisição: {response.ReasonPhrase}";
+                _logger.LogError(generalErrorMessage);
+                return (default, generalErrorMessage);
+            }
+        }
+        catch (HttpRequestException ex)
+        {
+            string errorMessage = $"Erro de requisição HTTP: {ex.Message}";
+            _logger.LogError(errorMessage);
+            return (default, errorMessage);
+        }
+        catch (JsonException ex)
+        {
+            string errorMessage = $"Erro na desserialização JSON: {ex.Message}";
+            _logger.LogError(errorMessage);
+            return (default, errorMessage);
+        }
+        catch (Exception ex)
+        {
+            string errorMessage = $"Erro inesperado: {ex.Message}";
+            _logger.LogError(errorMessage);
+            return (default, errorMessage);
+        }
+    }
+
+    public async Task<(bool Data, string? ErrorMessage)> AtualizaQuantidadeItemCarrinho(int produtoId, string acao)
+    {
+        try
+        {
+            var content = new StringContent(string.Empty, Encoding.UTF8, "application/json");
+            var response = await PutRequest($"api/ItensCarrinhoCompra?produtoId={produtoId}&acao={acao}", content);
+
+            if (response.IsSuccessStatusCode)
+            {
+                return (true, null);
+            }
+            else
+            {
+                if (response.StatusCode == HttpStatusCode.Unauthorized)
+                {
+                    string errorMessage = $"Unauthorized";
+                    _logger.LogError(errorMessage);
+                    return (false, errorMessage);
+                }
+                string generalErrorMessage = $"Erro na requisição: {response.ReasonPhrase}";
+                _logger.LogError(generalErrorMessage);
+                return (false, generalErrorMessage);
+            }
+        }
+        catch (HttpRequestException ex)
+        {
+            string errorMessage = $"Erro de requisição HTTP: {ex.Message}";
+            _logger.LogError(errorMessage);
+            return (false, errorMessage);
+        }
+    }
+
+    private async Task<HttpResponseMessage> PutRequest(string uri, HttpContent content)
+    {
+        var enderecoUrl = AppConfig.BaseUrl + uri;
+        try
+        {
+            AddAuthorizationHeader();
+            var result = await _httpClient.PutAsync(enderecoUrl, content);
+            return result;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"Erro ao enviar requisição PUT para {uri}: {ex.Message}");
+            return new HttpResponseMessage(HttpStatusCode.BadRequest);
+        }
+    }
+
+    private void AddAuthorizationHeader()
+    {
+        var token = Preferences.Get("accesstoken", string.Empty);
+
+        if (!string.IsNullOrEmpty(token))
+        {
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
         }
     }
 }
